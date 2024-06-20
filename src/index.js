0bb4a551c69eedf5bcff962f9d66d8225b6fd808
Not-7Myself <98397618+Not-7Myself@users.noreply.github.com>
2024-06-20 11:21:10 +0530

    h

--- src/index.js
+++ src/index.js
@@ -49,19 +49,40 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h2>Our Menu</h2>
       <Pizza />
     </div>
   );
 }
+
+function Footer() {
+  return <footer className="footer">We're currently Open!</footer>;
+}
+
 function Pizza() {
   return (
     <div>
       <img src="pizzas\spinaci.jpg" alt="spinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
